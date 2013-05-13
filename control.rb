cd4dd2f9cbd2730e04947cb54deb6423966994e7
Juan Ignacio Pumarino <jipumarino@gmail.com>
2013-05-13 12:51:15 -0400

    Logging enabled

--- control.rb
+++ control.rb
@@ -1,2 +1,2 @@
 require 'daemons'
-Daemons.run File.join(File.dirname(__FILE__), 'dashing_server_monitor.rb')
+Daemons.run File.join(File.dirname(__FILE__), 'dashing_server_monitor.rb'), log_output: true, backtrace: true
