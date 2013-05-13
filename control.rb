04962525ca7d12671574a7b3cac80bd3c391fb65
Juan Ignacio Pumarino <jipumarino@gmail.com>
2013-05-13 15:32:10 -0400

    Reiniciar si se cae

--- control.rb
+++ control.rb
@@ -1,2 +1,2 @@
 require 'daemons'
-Daemons.run File.join(File.dirname(__FILE__), 'dashing_server_monitor.rb'), log_output: true, backtrace: true
+Daemons.run File.join(File.dirname(__FILE__), 'dashing_server_monitor.rb'), log_output: true, backtrace: true, monitor: true
